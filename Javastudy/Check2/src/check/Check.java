890ac8eaf08d4b7c898ce47569337d009de52a49
Git-bashi <71607175+Git-bashi@users.noreply.github.com>
2021-01-13 23:21:53 +0900

    Update Check.java

--- Javastudy/Check2/src/check/Check.java
+++ Javastudy/Check2/src/check/Check.java
@@ -1,17 +1,19 @@
 package check;
 
+import constants.Constants;
+
 public class Check {
 	private static String firstName = "石橋";
 	private static String lastName = "亮典";
 
-	private void printName(String first, String last) {
-		System.out.println("printNameメソッド → " +  first + last);
+	private void printName(String firstName, String lastName) {
+		System.out.println("printNameメソッド → " +  firstName + lastName);
 	}
 
 	public static void main(String[] args) {
 
-		Pet pet = new Pet("java吉", "hoge");
-		RobotPet rp = new RobotPet("R2D2", "ルーク");
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
+		RobotPet rp = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
 		Check check = new Check();
 
 		check.printName(firstName, lastName);
